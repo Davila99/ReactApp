1b230ca0dae6b776ed8fdbdde168bc27a4335940
Davila99 <davilaeliseo453@gmail.com>
2021-10-05 09:58:08 -0600

    he editado el componente de avatar y agragado los primeors estilos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { ShowAlert } from "./components/Alerts";
+import {ShowAvatar} from "./components/Avatar"
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <ShowAlert altura="50px" ancho="600px" colorFondo="green" colorTexto="white" texto="Exitoso"></ShowAlert>
-        <ShowAlert altura="50px" ancho="600px" colorFondo="red" colorTexto="white" texto="Alert malo"></ShowAlert>
+        <ShowAvatar></ShowAvatar>
+        {/* <ShowAlert altura="50px" ancho="600px" colorFondo="green" colorTexto="white" texto="Exitoso"></ShowAlert>
+        <ShowAlert altura="50px" ancho="600px" colorFondo="red" colorTexto="white" texto="Alert malo"></ShowAlert> */}
       </header>
     </div>
   ); 
--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,11 +1,28 @@
-const Avatar=() =>{
+import avatar from './image/avatar.jpg'
+const ShowAvatar=() =>{
 
     return(
-<div >
-<div>
+<div style={{
+    backgroundColor:'#8B8B8B',
+    width:'400px',
+    height:'400px',
+    marginTop:'100px',
+    marginLeft:'40%'
+    
+}}>
+<img src={avatar} alt="" style={{
+    borderRadius:'50%',
+    width:'300px',
+    height:'300px',
+   marginTop:'50px',
+   marginLeft:'50px'
+}}/>
 
-</div>
 </div>
     );
 
+}
+
+export {
+    ShowAvatar
 }

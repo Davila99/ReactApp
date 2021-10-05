c8bcac075932a46fba25592cbee87b39e7bc214e
Davila99 <davilaeliseo453@gmail.com>
2021-10-05 16:33:45 -0600

    Editando en App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import { ShowAlert } from "./components/Alerts";
 import {ShowAvatar} from "./components/Avatar"
 
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ShowAlert altura="50px" ancho="600px" colorFondo="green" colorTexto="white" texto="Exitoso"></ShowAlert>
-        <ShowAlert altura="50px" ancho="600px" colorFondo="red" colorTexto="white" texto="Alert malo"></ShowAlert>
+        <ShowAlert altura="50px" ancho="600px" colorFondo="green" colorTexto="white" texto="Success!"></ShowAlert>
+        <ShowAlert altura="50px" ancho="600px" colorFondo="red" colorTexto="white" texto="Danger!!!"></ShowAlert>
 
-        
         <ShowAvatar urlImagen="https://i.pinimg.com/originals/64/40/2f/64402f6d6b0d3d0d6fccd7fa49cba0f3.jpg"nombre="" apellido=""></ShowAvatar>
         
       </header>

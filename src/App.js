e26990ffe2d496d60bbba7d552763b9809d4da4c
Davila99 <davilaeliseo453@gmail.com>
2021-10-05 11:55:43 -0600

    etitando la parte del avatar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ShowAvatar></ShowAvatar>
+        <ShowAvatar urlImagen="https://i.pinimg.com/originals/64/40/2f/64402f6d6b0d3d0d6fccd7fa49cba0f3.jpg"nombre="Eliseo" apellido="Davila"></ShowAvatar>
         {/* <ShowAlert altura="50px" ancho="600px" colorFondo="green" colorTexto="white" texto="Exitoso"></ShowAlert>
         <ShowAlert altura="50px" ancho="600px" colorFondo="red" colorTexto="white" texto="Alert malo"></ShowAlert> */}
       </header>
--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,28 +1,41 @@
 import avatar from './image/avatar.jpg'
-const ShowAvatar=() =>{
 
+const ShowAvatar=({urlImagen,nombre,apellido}) =>{
+    
+var cadenaNombre=nombre
+var firtLetterName=cadenaNombre.charAt(0)
+var cadenaApellido=apellido
+var firtLetterLastName=cadenaApellido.charAt(0)
+
+const ImagenUrl = urlImagen === '' ?   avatar:firtLetterName+firtLetterLastName
     return(
 <div style={{
     backgroundColor:'#8B8B8B',
     width:'400px',
     height:'400px',
-    marginTop:'100px',
+    marginTop:'300px',
     marginLeft:'40%'
     
 }}>
-<img src={avatar} alt="" style={{
+<img src={ImagenUrl} alt={ImagenUrl} style={{
     borderRadius:'50%',
     width:'300px',
     height:'300px',
    marginTop:'50px',
    marginLeft:'50px'
-}}/>
+}} />
 
 </div>
     );
 
 }
+// const NameInitialsAvatar=({nombre,apellido})=>{
 
+
+
+// }
 export {
     ShowAvatar
+    // NameInitialsAvatar
+    
 }
